bd7b91b5c631d9af864b19666666ec154cadfa74
Senthil <psenthil202@gmail.com>
2014-02-15 07:18:27 +0530

    Deploy script updated

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repository, "git@github.com:V1n0d/videocarpost.git"
 set :user, "root"
 set :scm_passphrase, ""
 set :branch, "master"
-set :deploy_via, :remote_cache
+set :deploy_via, :copy
 set :stages, ["staging", "development", "production"]
 set :default_stage, "development"
 default_run_options[:pty] = true
